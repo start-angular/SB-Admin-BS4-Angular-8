a1c639be91054724c0ea28e08b41b74511bbfd6f
Nitin Kumar Mishra <nitin@sahusoft.com>
2017-03-29 11:47:11 +0530

    fix dev running issue

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DropdownModule } from 'ng2-bootstrap';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponent } from './layout.component';
@@ -11,8 +9,6 @@ import { HeaderComponent, SidebarComponent } from '../shared';
 @NgModule({
     imports: [
         CommonModule,
-        DropdownModule.forRoot(),
-        NgbModule.forRoot(),
         LayoutRoutingModule
     ],
     declarations: [
