46b1d959913f1be5a49fbeb47ae0933be74d56a7
Nitin Kumar Mishra <nitin@sahusoft.com>
2017-03-30 20:31:07 +0530

    fix dropdwon issue

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponent } from './layout.component';
@@ -9,6 +10,7 @@ import { HeaderComponent, SidebarComponent } from '../shared';
 @NgModule({
     imports: [
         CommonModule,
+        NgbDropdownModule.forRoot(),
         LayoutRoutingModule
     ],
     declarations: [
