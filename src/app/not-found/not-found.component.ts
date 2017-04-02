83b79bd9d67c60931a63a15f19f2b70a89c7f947
Federico Baldassarre <baldassarre.fe@gmail.com>
2017-04-03 00:40:54 +0200

    Added a 404 page and its routing
    Added a 404 page that is similar in style to the login page
    and contains a link to navigate to the root of the application.

---
+++ src/app/not-found/not-found.component.ts
@@ -0,0 +1,8 @@
+import {Component} from "@angular/core";
+
+@Component({
+    templateUrl: './not-found.component.html',
+    styleUrls: ['not-found.component.scss']
+})
+export class NotFoundComponent {
+}
