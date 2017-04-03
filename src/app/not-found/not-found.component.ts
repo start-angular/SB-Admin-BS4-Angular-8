a91deb811002135c6402e98aa5f567ab19067e08
Nitin Kumar Mishra <nitin@sahusoft.com>
2017-04-03 09:00:18 +0530

    merge pull-request page not found

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,8 +1,8 @@
-import {Component} from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
+    selector: 'app-not-found',
     templateUrl: './not-found.component.html',
     styleUrls: ['not-found.component.scss']
 })
-export class NotFoundComponent {
-}
+export class NotFoundComponent { }
