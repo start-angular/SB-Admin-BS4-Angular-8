f4b18505992287e3470ebcd6d4dc3ce041a0962a
Nitin Kumar Mishra <nitin@sahusoft.com>
2017-04-03 11:34:31 +0530

    change restart link in not found module

--- src/app/not-found/not-found.module.ts
+++ src/app/not-found/not-found.module.ts
@@ -1,10 +1,14 @@
-import {NgModule} from "@angular/core";
-import {NotFoundComponent} from "./not-found.component";
-import {NotFoundRoutingModule} from "./not-found-routing.module";
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { NotFoundComponent } from './not-found.component';
+import { NotFoundRoutingModule } from './not-found-routing.module';
 
 @NgModule({
-  imports: [NotFoundRoutingModule],
-  declarations: [NotFoundComponent]
+    imports: [
+        NotFoundRoutingModule,
+        RouterModule
+    ],
+    declarations: [NotFoundComponent]
 })
-export class NotFoundModule {
-}
+export class NotFoundModule {}
