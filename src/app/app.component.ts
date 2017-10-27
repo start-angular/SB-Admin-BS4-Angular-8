bcf3eeca71b9a82fec9fd810c976c29bc7ffed69
Moritz Schmidt <moritz@mmnx.de>
2017-10-27 23:21:15 +0200

    - Added language switch - Added translation for german language in all translations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,9 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class AppComponent {
     constructor(private translate: TranslateService) {
-        translate.addLangs(['en', 'fr', 'ur', 'es', 'it', 'fa']);
+        translate.addLangs(['en', 'fr', 'ur', 'es', 'it', 'fa', 'de']);
         translate.setDefaultLang('en');
         const browserLang = translate.getBrowserLang();
-        translate.use(browserLang.match(/en|fr|ur|es|it|fa/) ? browserLang : 'en');
+        translate.use(browserLang.match(/en|fr|ur|es|it|fa|de/) ? browserLang : 'en');
     }
 }
