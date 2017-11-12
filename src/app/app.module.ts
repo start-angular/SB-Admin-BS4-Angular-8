3c455ea6470cca57969dcf03fd2508669982409e
Nitin Kumar Mishra <nitin.kr.mr@gmail.com>
2017-11-12 18:37:15 +0530

    fix build path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { AuthGuard } from './shared';
 // AoT requires an exported function for factories
 export function createTranslateLoader(http: HttpClient) {
     // for development
-    // return new TranslateHttpLoader(http, '/start-angular/SB-Admin-BS4-Angular-4/master/dist/assets/i18n/', '.json');
+    // return new TranslateHttpLoader(http, '/start-angular/SB-Admin-BS4-Angular-5/master/dist/assets/i18n/', '.json');
     return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
