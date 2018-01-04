1446e59daddb4b4ebb8750fa25af7bdd56793c28
Alpesh <alpesh@sahusoft.com>
2018-01-04 15:57:53 +0530

    Logout and other option added on sidebar for mobile/small screens.

--- src/app/layout/components/sidebar/sidebar.component.ts
+++ src/app/layout/components/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-sidebar',
@@ -8,6 +10,24 @@ import { Component, OnInit } from '@angular/core';
 export class SidebarComponent {
     isActive: boolean = false;
     showMenu: string = '';
+    pushRightClass: string = 'push-right';
+
+    constructor(private translate: TranslateService, public router: Router) {
+        this.translate.addLangs(['en', 'fr', 'ur', 'es', 'it', 'fa', 'de']);
+        this.translate.setDefaultLang('en');
+        const browserLang = this.translate.getBrowserLang();
+        this.translate.use(browserLang.match(/en|fr|ur|es|it|fa|de/) ? browserLang : 'en');
+
+        this.router.events.subscribe(val => {
+            if (
+                val instanceof NavigationEnd &&
+                window.innerWidth <= 992 &&
+                this.isToggled()
+            ) {
+                this.toggleSidebar();
+            }
+        });
+    }
 
     eventCalled() {
         this.isActive = !this.isActive;
@@ -19,5 +39,28 @@ export class SidebarComponent {
         } else {
             this.showMenu = element;
         }
+    }
+
+    isToggled(): boolean {
+        const dom: Element = document.querySelector('body');
+        return dom.classList.contains(this.pushRightClass);
+    }
+
+    toggleSidebar() {
+        const dom: any = document.querySelector('body');
+        dom.classList.toggle(this.pushRightClass);
+    }
+
+    rltAndLtr() {
+        const dom: any = document.querySelector('body');
+        dom.classList.toggle('rtl');
+    }
+
+    changeLang(language: string) {
+        this.translate.use(language);
+    }
+
+    onLoggedout() {
+        localStorage.removeItem('isLoggedin');
     }
 }
