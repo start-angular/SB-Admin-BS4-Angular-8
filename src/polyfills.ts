79cad3b08561fbffc710f3ad72b68bc00d11a8d6
Nitin Mishra <nitinkrmr@gmail.com>
2022-02-16 20:34:03 +0530

    angular 12 update

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -7,7 +7,7 @@ import 'core-js/es/reflect';
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone'; // Included with Angular CLI.
+import 'zone.js'; // Included with Angular CLI.
 /**
  * This file includes polyfills needed by Angular and is loaded before the app.
  * You can add your own extra polyfills to this file.
