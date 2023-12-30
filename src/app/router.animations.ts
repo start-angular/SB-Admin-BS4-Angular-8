a9a7424022eee9611a5a2c81cc67fa90f453a86f
Nitin Mishra <nitinkrmr@gmail.com>
2023-12-30 10:37:06 +0530

    Replace noTransition with fadeInAnimation
    The code in router.animations.ts has been modified to replace the `noTransition` function with `fadeInAnimation` in the `routerTransition` function. The `fadeInAnimation` feature introduces a smooth transition and enhances the user interface experience.

--- src/app/router.animations.ts
+++ src/app/router.animations.ts
@@ -1,11 +1,20 @@
 import { animate, state, style, transition, trigger } from '@angular/animations';
 
 export function routerTransition() {
-    return noTransition();
+    return fadeInAnimation();
 }
 
 export function noTransition() {
     return trigger('routerTransition', []);
+}
+
+export function fadeInAnimation() {
+    return trigger('routerTransition', [
+        state('void', style({})),
+        state('*', style({})),
+        transition(':enter', [style({ opacity: 0 }), animate(500, style({ opacity: 1 }))]),
+        transition(':leave', [animate(500, style({ opacity: 0 }))])
+    ]);
 }
 
 export function slideToRight() {
